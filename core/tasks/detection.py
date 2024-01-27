318e2acd000557d15972d2070c841507d4c0435e
周钰坤 <90625606+1zeryu@users.noreply.github.com>
2024-01-27 12:12:08 +0800

    update

--- core/tasks/detection.py
+++ core/tasks/detection.py
@@ -3,13 +3,10 @@ import hydra.utils
 from .base_task import  BaseTask
 from core.data.vision_dataset import VisionData
 from core.data.parameters import PData
-from core.utils.utils import *
-import torch.nn as nn
-import datetime
-from core.utils import *
-import glob
-import omegaconf
-import json
+from torchvision.io.image import read_image
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights, fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
+from torchvision.utils import draw_bounding_boxes
+from torchvision.transforms.functional import to_pil_image
 
 
 # object detection task
@@ -20,8 +17,19 @@ class ODTask(BaseTask):
         self.eval_loader = self.task_data.val_dataloader()
         self.test_loader = self.task_data.test_dataloader()
 
+    def build_model(self):
+        if self.cfg.model == 'faster_rcnn_v1':
+            weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT
+            model = fasterrcnn_resnet50_fpn(weights=weights, box_score_thresh=0.9)
+        elif self.cfg.model == 'faster_rcnn_v2':
+            weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+            model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.9)
+        else:
+            raise NotImplementedError
+        return model
+
     def init_task_data(self):
-        pass
+        return
 
     def set_param_data(self):
         pass

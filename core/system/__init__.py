c1e2f7e858a574f6b00b959add09931c9435e14e
周钰坤 <90625606+1zeryu@users.noreply.github.com>
2024-02-23 16:50:59 +0800

    update

--- core/system/__init__.py
+++ core/system/__init__.py
@@ -3,12 +3,10 @@ from .ddpm import *
 from .encoder import *
 from .vae import VAESystem
 from .ae_ddpm import AE_DDPM
-from .explore import Explore
 
 systems = {
     'encoder': EncoderSystem,
     'ddpm': DDPM,
     'vae': VAESystem,
     'ae_ddpm': AE_DDPM,
-    'explore': Explore,
 }

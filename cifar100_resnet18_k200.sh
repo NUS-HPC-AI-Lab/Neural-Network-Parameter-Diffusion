af757eec3bdd99176ae03e14a34b507c9e826577
YuKun Zhou <90625606+1zeryu@users.noreply.github.com>
2024-02-23 16:47:53 +0800

    Update cifar100_resnet18_k200.sh

--- cifar100_resnet18_k200.sh
+++ cifar100_resnet18_k200.sh
@@ -1,2 +1,2 @@
-python task_training # get the model data for training p-diff
+python task_training.py # get the model data for training p-diff
 python train_p_diff --task cifar100 --system ae_ddpm 
